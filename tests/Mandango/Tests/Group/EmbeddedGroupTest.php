3786b2d3231b712f54671c69728897a74606ddd5
pablodip <pablodip@gmail.com>
2011-04-24 22:18:26 +0100

    changed license to MIT and files header

--- tests/Mandango/Tests/Group/EmbeddedGroupTest.php
+++ tests/Mandango/Tests/Group/EmbeddedGroupTest.php
@@ -1,22 +1,12 @@
 <?php
 
 /*
- * Copyright 2010 Pablo Díez <pablodip@gmail.com>
- *
  * This file is part of Mandango.
  *
- * Mandango is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * (c) Pablo Díez <pablodip@gmail.com>
  *
- * Mandango is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with Mandango. If not, see <http://www.gnu.org/licenses/>.
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
  */
 
 namespace Mandango\Tests\Group;
