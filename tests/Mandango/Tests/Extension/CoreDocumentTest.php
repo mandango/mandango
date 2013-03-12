0fc5fcc4bbd10cdc8454273b316e3787b2e57896
Ahmed Nasser <ahmed.nasser@jidari.com>
2013-03-12 13:46:16 +0200

    Fix for issue #19

--- tests/Mandango/Tests/Extension/CoreDocumentTest.php
+++ tests/Mandango/Tests/Extension/CoreDocumentTest.php
@@ -308,7 +308,7 @@ class CoreDocumentTest extends TestCase
         $this->assertFalse($author1->isModified());
         $this->assertFalse($author2->isModified());
     }
-
+    
     public function testSaveReferencesReferencesMany()
     {
         $articleCategories = array(
@@ -332,6 +332,25 @@ class CoreDocumentTest extends TestCase
             $this->assertFalse($category->isModified());
         }
     }
+    
+    public function testSaveReferencesReferencesManyInEmbedded()
+    {
+        $article = $this->mandango->create('Model\Article');
+        $category = $this->mandango->create('Model\Category')->setName('c1');
+        $comment = $this->mandango->create('Model\Comment')->setName('foo');
+        
+
+        $comment->addCategories($category);        
+        $article->addComments($comment);
+
+        $article->save();
+        
+        foreach($article->getComments() as $c)
+        {
+            $this->assertEquals(count($c->getCategories()), 1);
+        }
+    }
+
 
     public function testEmbeddedsOneSettersGetters()
     {
